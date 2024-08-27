396e42a41fb70c58bb3de49d799f316de94940c5
Felipe C. Valverde <felipecaetanodev@gmail.com>
2024-08-27 11:03:07 -0300

    add

--- src/RoutesApp.jsx
+++ src/RoutesApp.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Error from './pages/Error';
 import ProjectsLibrary from './pages/Library';
 import Project01 from './pages/Project-01/index';
+import Project02 from './pages/Project-02/index';
 
 export default function RoutesApp() {
   const BackdropAction = ({ children }) => {
@@ -12,6 +13,7 @@ export default function RoutesApp() {
     <BackdropAction>
       <Routes>
         <Route exact path='/project-01' element={<Project01 />} />
+        <Route exact path='/project-02' element={<Project02 />} />
         <Route exact path='/' element={<ProjectsLibrary />} />
         <Route exact path='*' element={<Error />} />
       </Routes>
